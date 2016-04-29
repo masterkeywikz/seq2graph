f3b1df221d729e3a257e1e19cda7ae4db086baae
qzyou <ultimateyou@gmail.com>
2016-04-29 10:23:18 -0400

    Fix training NaN error.

--- src/script_main_att.sh
+++ src/script_main_att.sh
@@ -8,4 +8,6 @@ gid=1
 if [ $# -ge 2 ]; then
     gid=$2
 fi
+#THEANO_FLAGS="optimizer=fast_compile,mode=NanGuardMode,device=gpu$gid" python script_main_att.py $1
+#THEANO_FLAGS="mode=NanGuardMode,device=gpu$gid" python script_main_att.py $1
 THEANO_FLAGS="device=gpu$gid" python script_main_att.py $1
