b484adea18b619d1fd102c67eaa8905551652d4f
Xiaochang Peng <xpeng@j02.cs.rochester.edu>
2016-08-30 20:47:54 -0400

    run scripts

--- amr2seq/data_prep/run.sh
+++ amr2seq/data_prep/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#python ./categorize_amr.py --data_dir ../../train --run_dir run_dir --stats_dir stats
-python ./categorize_amr.py --data_dir ../../train --run_dir run_dir --stats_dir stats --use_stats
+#python ./categorize_amr.py --data_dir ../../train --use_lemma --run_dir run_dir --stats_dir stats --use_stats --parallel
+python ./categorize_amr.py --data_dir ../../dev --use_lemma --run_dir run_dir --stats_dir stats --use_stats --map_file ./run_dir/train_map
--- amr2seq/run_seq.sh
+++ amr2seq/run_seq.sh
@@ -7,6 +7,6 @@ INPUT=$1
 tail -n +2 $INPUT | sed 's/> //g' > $INPUT.tmp
 
 #If AMR to sequence
-python amr2seq.py --data_dir ../dev --amrseq_file $INPUT.tmp --seq2amr --version 0.0
+python amr2seq.py --data_dir ../dev --amrseq_file $INPUT.tmp --seq2amr --version 1.1
 
 rm $INPUT.tmp
