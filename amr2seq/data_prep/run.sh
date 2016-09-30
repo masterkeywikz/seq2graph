62a8c5d678f33b26fd78f0bc092279fefbe876a9
Xiaochang Peng <xiaochap@Xiaochangs-MacBook-Pro.local>
2016-09-30 21:03:12 +0800

    committed eval data

--- amr2seq/data_prep/run.sh
+++ amr2seq/data_prep/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #python ./categorize_amr.py --data_dir ../../train --use_lemma --run_dir run_dir --stats_dir stats --use_stats --parallel
-python ./categorize_amr.py --data_dir ../../dev --use_lemma --run_dir run_dir --stats_dir stats --use_stats --map_file ./run_dir/train_map
+#python ./categorize_amr.py --data_dir ../../dev --use_lemma --run_dir run_dir --stats_dir stats --use_stats --map_file ./run_dir/train_map
+python ./categorize_amr.py --data_dir ../../eval --use_lemma --run_dir run_dir --stats_dir stats --use_stats --map_file ./run_dir/train_map
